a3ca4f9e9fbfef6b671b6dcf67ba84f6d34f3b30
Riccardo Marino <rmarino@apple.com>
2026-03-09 11:27:09 -0700

    fix: widen GroupPolicy type in group-policy-warnings to include members

--- src/channels/plugins/group-policy-warnings.ts
+++ src/channels/plugins/group-policy-warnings.ts
@@ -64,7 +64,7 @@ export function buildOpenGroupPolicyConfigureRouteAllowlistWarning(params: {
 
 export function collectOpenGroupPolicyRestrictSendersWarnings(
   params: Parameters<typeof buildOpenGroupPolicyRestrictSendersWarning>[0] & {
-    groupPolicy: "open" | "allowlist" | "disabled";
+    groupPolicy: GroupPolicy;
   },
 ): string[] {
   if (params.groupPolicy !== "open") {
